6903e59e41c5a9d46ecf73dcffa0ecd94cd3e6b6
GerarNava <110059931+GerarNava@users.noreply.github.com>
2022-08-24 19:55:48 -0600

    iSe dio un estilo al formulario

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -4,29 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles.css">
     <title>Document</title>
 </head>
 <body>
-    <h1> Ingreso de Post</h1>
+    <h1 class="titulo"> Ingreso de Post</h1>
     <br>
     <br>
     <form action="{{route('post.store')}}"method="post">
     @csrf
-    <label for="">Título</label>
-    <input type="text" name="title">
+    <div class="colNom" >
+        <label for="">Título</label><br>
+        <input class="anchNom" type="text" name="title">
+    </div>
 
-    <label for="">Url corta</label>
-    <input type="text" name="slug">
+    <div class="colUrl" >
+        <label for="">Url corta</label><br>
+        <input class="anchNom" type="text" name="slug">
+    </div>
 
-    <label for="">Conteniedo</label>
-    <textarea name="contenet"></textarea>
+    <div class="ConCont">  
+        <label for="">Conteniedo</label><br>
+        <textarea class="anchNom2" name="contenet"></textarea>
+    </div>   
     
-    <label for="">Descripcion</label>
-    <textarea name="Description"></textarea>
+    <div class="ConDesc">
+        <label for="">Descripcion</label><br>
+        <textarea class="anchNom2" name="Description"></textarea>
+    </div>
 
 
-
-    <button type="submit">Enviar</button>
+    <button class="btn-Enviar" type="submit">Enviar</button>
 </form>    
 </body>
 </html>
